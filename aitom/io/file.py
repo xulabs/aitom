234774d8cf9d325d9b4626ce1bf1cde4bbeccb9e
Min Xu <xumin100@gmail.com>
2019-06-30 20:19:37 -0400

    added mrc file reading and writing

---
+++ aitom/io/file.py
@@ -0,0 +1,21 @@
+'''
+Functions for reading and writing common files
+'''
+
+import aitom.tomominer.io.mrcfile_proxy as TIM
+
+def read_mrc_data(path, show_progress=False):
+    #return read_mrc(path=path, show_progress=show_progress)['value']
+    return TIM.read_data(path)
+
+import aitom.tomominer.io.file as ATIF
+def read_mrc_header(path):
+    return ATIF.read_mrc(path=path, read_data=False)['header']
+
+
+def put_mrc(mrc, path, overwrite=False):
+    put_mrc_data(mrc,path, overwrite=overwrite)
+
+def put_mrc_data(mrc,path, overwrite=False):
+    TIM.write_data(data=mrc,path=path, overwrite=overwrite)
+
---
+++ aitom/io/mrcfile_proxy.py
@@ -0,0 +1,41 @@
+'''
+access mrc files using mrcfile package
+
+'''
+
+import numpy as N
+import mrcfile
+
+def read(path):
+    with mrcfile.open(path, 'r') as m:
+        header = m.header
+        data = m.data
+        assert data.ndim == 3  # only for 3D array
+        data = data.transpose([2, 1, 0])        # this is according to tomominer.image.vol.eman2_util.em2numpy
+
+    return {'header':header, 'data': data}
+
+def read_data(path):
+    return read(path)['data']
+
+
+def read_header(path):
+    from mrcfile.mrcinterpreter import MrcInterpreter
+
+    mi = MrcInterpreter()
+    mi._iostream = open(path, 'rb')
+    mi._read_header()
+    mi._iostream.close()
+
+    return mi.header
+
+
+def write_data(data, path, overwrite=False):
+    assert data.ndim == 3  # only for 3D array
+
+    data = data.astype(N.float32)
+    data = data.transpose([2,1,0])        # this is according to tomominer.image.vol.eman2_util.numpy2em
+    with mrcfile.new(path, overwrite=overwrite) as m:
+        m.set_data(data)
+
+
