f739d360ad6843e0dfeb86cb9ac62650f6a0b011
Min Xu <xumin100@gmail.com>
2019-08-25 10:36:15 -0400

    add placeholder of template search pipeline

---
+++ tutorials/007_001_template_search.py
@@ -0,0 +1,9 @@
+'''
+a pipeline of template search based localization of specific macromolecular compleses
+
+Setps:
+1) particle picking
+2) autoencoder
+3) subtomogram alignment based template search
+'''
+
