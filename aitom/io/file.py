c6c79abeeabf499297533d0028cf8e99653618ac
xulabs <xumin100@gmail.com>
2019-09-10 16:17:13 +0100

    Update file.py

--- aitom/io/file.py
+++ aitom/io/file.py
@@ -13,7 +13,9 @@ def read_mrc_header(path):
     return ATIF.read_mrc(path=path, read_data=False)['header']
 
 '''
-Calculate voxel spacing from the mrc header obtained from read_mrc_header
+Calculate voxel spacing 
+from the mrc header obtained from read_mrc_header
+in Angstrom unit
 '''
 def get_mrc_voxel_spacing(im):
     return (im['header']['MRC']['xlen'] / im['header']['MRC']['nx'])
