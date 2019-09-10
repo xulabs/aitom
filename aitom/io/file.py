4cdea24fbeb14dd549bf6c2c5ad095092b00dca4
xulabs <xumin100@gmail.com>
2019-09-10 16:15:02 +0100

    Update file.py

--- aitom/io/file.py
+++ aitom/io/file.py
@@ -12,6 +12,11 @@ import aitom.tomominer.io.file as ATIF
 def read_mrc_header(path):
     return ATIF.read_mrc(path=path, read_data=False)['header']
 
+'''
+Calculate voxel spacing from the mrc header obtained from read_mrc_header
+'''
+def get_mrc_voxel_spacing(im):
+    return (im['header']['MRC']['xlen'] / im['header']['MRC']['nx'])
 
 def put_mrc(mrc, path, overwrite=False):
     put_mrc_data(mrc,path, overwrite=overwrite)
