e4daa2bd5cbfffc5320424ee38aaaa8b17aa86fa
Min Xu <xumin100@gmail.com>
2019-10-02 12:48:23 -0400

    added simulation code

---
+++ aitom/simulation/tomogram/single_bounding_sphere/boundingSphere.py
@@ -0,0 +1,467 @@
+import mrcfile
+import numpy as np
+import scipy
+from scipy.spatial import ConvexHull
+import sys
+
+
+def read_mrc(mrc_path):
+    with mrcfile.open(mrc_path) as mrc:
+        v = mrc.data
+    return v
+
+
+def ldivide(array, vector):
+    return np.linalg.solve(array, vector)
+
+
+'''def convexHull(v, L):
+    points = []
+    density_max = v.max()
+    contour_level = L * density_max
+    for ijk in np.ndindex(v.shape):
+        if v[ijk] >= contour_level:
+            points.append([float(ijk[0]), float(ijk[1]), float(ijk[2])])
+
+    hull = ConvexHull(points)
+    return hull, points'''
+
+
+
+from scipy import linalg
+
+
+def fit_sphere_2_points(array):
+    """Fit a sphere to a set of 2, 3, or at most 4 points in 3D space. Note that
+    point configurations with 3 collinear or 4 coplanar points do not have
+    well-defined solutions (i.e., they lie on spheres with inf radius).
+
+    - X     : M-by-3 array of point coordinates, where M<=4.
+    - R     : radius of the sphere. R=Inf when the sphere is undefined, as
+                specified above.
+    - C     : 1-by-3 vector specifying the centroid of the sphere.
+                C=nan(1,3) when the sphere is undefined, as specified above.
+
+    Matlab code author: Anton Semechko (a.semechko@gmail.com)
+    Date: Dec.2014"""
+
+    N = len(array)
+
+    if N > 4:
+        print('Input must a N-by-3 array of point coordinates, with N<=4')
+        return
+
+    # Empty set
+    elif N == 0:
+        R = np.nan
+        C = np.full(3, np.nan)
+        return R, C
+
+    # A single point
+    elif N == 1:
+        R = 0.
+        C = array[0]
+        return R, C
+
+    # Line segment
+    elif N == 2:
+        R = np.linalg.norm(array[1] - array[0]) / 2
+        C = np.mean(array, axis=0)
+        return R, C
+
+    else:  # 3 or 4 points
+        # Remove duplicate vertices, if there are any
+        uniq, index = np.unique(array, axis=0, return_index=True)
+        array_nd = uniq[index.argsort()]
+        if not np.array_equal(array, array_nd):
+            #print("found duplicate")
+            #print(array_nd)
+            R, C = fit_sphere_2_points(array_nd)
+            return R, C
+
+        tol = 0.01  # collinearity/co-planarity threshold (in degrees)
+        if N == 3:
+            # Check for collinearity
+            D12 = array[1] - array[0]
+            D12 = D12 / np.linalg.norm(D12)
+            D13 = array[2] - array[0]
+            D13 = D13 / np.linalg.norm(D13)
+
+            chk = np.clip(np.abs(np.dot(D12, D13)), 0., 1.)
+            if np.arccos(chk) / np.pi * 180 < tol:
+                R = np.inf
+                C = np.full(3, np.nan)
+                return R, C
+
+            # Make plane formed by the points parallel with the xy-plane
+            n = np.cross(D13, D12)
+            n = n / np.linalg.norm(n)
+            ##print("n", n)
+            r = np.cross(n, np.array([0, 0, 1]))
+            r = np.arccos(n[2]) * r / np.linalg.norm(r)  # Euler rotation vector
+            ##print("r", r)
+            Rmat = linalg.expm(np.array([
+                [0., -r[2], r[1]],
+                [r[2], 0., -r[0]],
+                [-r[1], r[0], 0.]
+            ]))
+            ##print("Rmat", Rmat)
+            # Xr = np.transpose(Rmat*np.transpose(array))
+            Xr = np.transpose(np.dot(Rmat, np.transpose(array)))
+            ##print("Xr", Xr)
+
+            # Circle centroid
+            x = Xr[:, :2]
+            A = 2 * (x[1:] - np.full(2, x[0]))
+            b = np.sum((np.square(x[1:]) - np.square(np.full(2, x[0]))), axis=1)
+            C = np.transpose(ldivide(A, b))
+
+            # Circle radius
+            R = np.sqrt(np.sum(np.square(x[0] - C)))
+
+            # Rotate centroid back into the original frame of reference
+            C = np.append(C, [np.mean(Xr[:, 2])], axis=0)
+            C = np.transpose(np.dot(np.transpose(Rmat), C))
+            return R, C
+
+        # If we got to this point then we have 4 unique, though possibly co-linear
+        # or co-planar points.
+        else:
+            # Check if the the points are co-linear
+            D12 = array[1] - array[0]
+            D12 = D12 / np.linalg.norm(D12)
+            D13 = array[2] - array[0]
+            D13 = D13 / np.linalg.norm(D13)
+            D14 = array[3] - array[0]
+            D14 = D14 / np.linalg.norm(D14)
+
+            chk1 = np.clip(np.abs(np.dot(D12, D13)), 0., 1.)
+            chk2 = np.clip(np.abs(np.dot(D12, D14)), 0., 1.)
+            if np.arccos(chk1) / np.pi * 180 < tol or np.arccos(chk2) / np.pi * 180 < tol:
+                R = np.inf
+                C = np.full(3, np.nan)
+                return R, C
+
+            # Check if the the points are co-planar
+            n1 = np.linalg.norm(np.cross(D12, D13))
+            n2 = np.linalg.norm(np.cross(D12, D14))
+
+            chk = np.clip(np.abs(np.dot(n1, n2)), 0., 1.)
+            if np.arccos(chk) / np.pi * 180 < tol:
+                R = np.inf
+                C = np.full(3, np.nan)
+                return R, C
+
+            # Centroid of the sphere
+            A = 2 * (array[1:] - np.full(len(array) - 1, array[0]))
+            b = np.sum((np.square(array[1:]) - np.square(np.full(len(array) - 1, array[0]))), axis=1)
+            C = np.transpose(ldivide(A, b))
+
+            # Radius of the sphere
+            R = np.sqrt(np.sum(np.square(array[0] - C), axis=0))
+
+            return R, C
+
+
+def permute_dims(array, dims):
+    return np.transpose(np.expand_dims(array, axis=max(dims)), dims)
+
+
+def ldivide(array, vector):
+    return np.linalg.solve(array, vector)
+def fit_2_points(vertices):
+    num_vertices = len(vertices)
+
+    tol = 0.01
+
+    if num_vertices == 0:
+        R = np.nan
+        C = np.array([np.nan, np.nan, np.nan])
+        return R, C
+    elif num_vertices == 1:
+        R = 0
+        C = vertices[0]
+        return R, C
+    elif num_vertices == 2:
+        R = np.linalg.norm(vertices[1] - vertices[0]) / 2
+        C = np.mean(vertices, axis=0)
+        return R, C
+    elif num_vertices == 3:
+        uniq, index = np.unique(vertices, axis=0, return_index=True)
+        no_dup = uniq[index.argsort()]
+
+        if not num_vertices == len(no_dup):
+            R, C = fit_2_points(no_dup)
+            return R, C
+        else:
+            dist1 = vertices[1] - vertices[0]
+            dist1 = dist1 / np.linalg.norm(dist1)
+            dist2 = vertices[2] - vertices[0]
+            dist2 = dist2 / np.linalg.norm(dist2)
+
+            chk = np.clip(np.abs(np.dot(dist1, dist2)), 0., 1.)
+
+            if np.arccos(chk) / np.pi*180 < 0.01:
+                R = np.inf
+                C = np.array([np.nan, np.nan, np.nan])
+                return R, C
+
+            n = np.cross(dist1, dist2)
+            n = n / np.linalg.norm(n)
+
+            r = np.cross(n, np.array([0, 0, 1]))
+            r = np.arccos(n[2]) * r / np.linalg.norm(r)
+
+            Rmat = scipy.linalg.expm(np.array([
+                [0., -r[2], r[1]],
+                [r[2], 0., -r[0]],
+                [-r[1], r[0], 0.]
+            ]))
+
+            Xr = np.transpose(np.dot(Rmat, np.transpose(vertices)))
+
+            x = Xr[:, :2]
+            A = 2 * (x[1:] - np.full(2, x[0]))
+            b = np.sum((np.square(x[1:]) - np.square(np.full(2, x[0]))), axis=1)
+            C = np.transpose(ldivide(A, b))
+
+            R = np.sqrt(np.sum(np.square(x[0] - C)))
+
+            C = np.append(C, [np.mean(Xr[:, 2])], axis=0)
+            C = np.transpose(np.dot(np.transpose(Rmat), C))
+    else:
+        D12 = vertices[1] - vertices[0]
+        D12 = D12 / np.linalg.norm(D12)
+        D13 = vertices[2] - vertices[0]
+        D13 = D13 / np.linalg.norm(D13)
+        D14 = vertices[3] - vertices[0]
+        D14 = D14 / np.linalg.norm(D14)
+
+        chk1 = np.clip(np.abs(np.dot(D12, D13)), 0., 1.)
+        chk2 = np.clip(np.abs(np.dot(D12, D14)), 0., 1.)
+        if np.arccos(chk1) / np.pi * 180 < tol or np.arccos(chk2) / np.pi * 180 < tol:
+            R = np.inf
+            C = np.full(3, np.nan)
+            return R, C
+
+        # Check if the the points are co-planar
+        n1 = np.linalg.norm(np.cross(D12, D13))
+        n2 = np.linalg.norm(np.cross(D12, D14))
+
+        chk = np.clip(np.abs(np.dot(n1, n2)), 0., 1.)
+        if np.arccos(chk) / np.pi * 180 < tol:
+            R = np.inf
+            C = np.full(3, np.nan)
+            return R, C
+
+        # Centroid of the sphere
+        A = 2 * (vertices[1:] - np.full(len(vertices) - 1, vertices[0]))
+        b = np.sum((np.square(vertices[1:]) - np.square(np.full(len(vertices) - 1, vertices[0]))), axis=1)
+        C = np.transpose(ldivide(A, b))
+
+        # Radius of the sphere
+        R = np.sqrt(np.sum(np.square(vertices[0] - C), axis=0))
+
+        return R, C
+
+
+def permute_dims(array, dims):
+    return np.transpose( np.expand_dims(array, axis=max(dims)), dims )
+
+
+def B_min_sphere(P, B):
+    eps = 1E-6
+    if len(B) == 4 or len(P) == 0:
+        R, C = fit_2_points(B)  # fit sphere to boundary points
+        return R, C, P
+
+    # Remove the last (i.e., end) point, p, from the list
+    P_new = P[:-1].copy()
+    p = P[-1].copy()
+
+    # Check if p is on or inside the bounding sphere. If not, it must be
+    # part of the new boundary.
+    R, C, P_new = B_min_sphere(P_new, B)
+    if np.isnan(R) or np.isinf(R) or R < eps:
+        chk = True
+    else:
+        chk = np.linalg.norm(p - C) > (R + eps)
+
+    if chk:
+        if len(B) == 0:
+            B = np.array([p])
+        else:
+            B = np.array(np.insert(B, 0, p, axis=0))
+        R, C, _ = B_min_sphere(P_new, B)
+        P = np.insert(P_new.copy(), 0, p, axis=0)
+    return R, C, P
+
+
+def exact_min_bound_sphere_3D(array):
+    """
+ Compute exact minimum bounding sphere of a 3D point cloud (or a
+ triangular surface mesh) using Welzl's algorithm.
+
+   - X     : M-by-3 list of point co-ordinates or a triangular surface
+             mesh specified as a TriRep object.
+   - R     : radius of the sphere.
+   - C     : 1-by-3 vector specifying the centroid of the sphere.
+   - Xb    : subset of X, listing K-by-3 list of point coordinates from
+             which R and C were computed. See function titled
+             'FitSphere2Points' for more info.
+
+ REREFERENCES:
+ [1] Welzl, E. (1991), 'Smallest enclosing disks (balls and ellipsoids)',
+     Lecture Notes in Computer Science, Vol. 555, pp. 359-370
+
+ Matlab code author: Anton Semechko (a.semechko@gmail.com)
+ Date: Dec.2014"""
+
+    # Get the convex hull of the point set
+    hull = ConvexHull(array)
+    hull_array = [array[i] for i in hull.vertices]
+    hull_array = np.unique(hull_array, axis=0)
+    #print(len(hull_array))
+
+    # Randomly permute the point set
+    hull_array = np.random.permutation(hull_array)
+
+    if len(hull_array) <= 4:
+        R, C = fit_sphere_2_points(hull_array)
+        return R, C, hull_array
+
+    elif len(hull_array) < 1000:
+        try:
+            R, C, _ = B_min_sphere(hull_array, [])
+
+            # Coordiantes of the points used to compute parameters of the
+            # minimum bounding sphere
+            D = np.sum(np.square(hull_array - C), axis=1)
+            idx = np.argsort(D - R ** 2)
+            D = D[idx]
+            Xb = hull_array[idx[:5]]
+            D = D[:5]
+            Xb = Xb[D < 1E-6]
+            idx = np.argsort(Xb[:, 0])
+            Xb = Xb[idx]
+            return R, C, Xb
+        except:
+            raise Exception
+    else:
+        M = len(hull_array)
+        dM = min([M // 4, 300])
+        # unnecessary ?
+        #		res = M % dM
+        #		n = np.ceil(M/dM)
+        #		idx = dM * np.ones((1, n))
+        #		if res > 0:
+        #			idx[-1] = res
+        #
+        #		if res <= 0.25 * dM:
+        #			idx[n-2] = idx[n-2] + idx[n-1]
+        #			idx = idx[:-1]
+        #			n -= 1
+
+        hull_array = np.array_split(hull_array, dM)
+        Xb = np.empty([0, 3])
+        for i in range(len(hull_array)):
+            R, C, Xi = B_min_sphere(np.vstack([Xb, hull_array[i]]), [])
+
+            # 40 points closest to the sphere
+            D = np.abs(np.sqrt(np.sum((Xi - C) ** 2, axis=1)) - R)
+            idx = np.argsort(D, axis=0)
+            Xb = Xi[idx[:40]]
+
+        D = np.sort(D, axis=0)[:4]
+        #print(Xb)
+        #print(D)
+        #print(np.where(D / R < 1e-3)[0])
+        Xb = np.take(Xb, np.where(D / R < 1e-3)[0], axis=0)
+        Xb = np.sort(Xb, axis=0)
+        #print(Xb)
+
+        return R, C, Xb
+
+
+def B_min_sphere(P, B):
+    eps = 1E-6
+    if len(B) == 4 or len(P) == 0:
+        R, C = fit_sphere_2_points(B)  # fit sphere to boundary points
+        return R, C, P
+
+    # Remove the last (i.e., end) point, p, from the list
+    P_new = P[:-1].copy()
+    p = P[-1].copy()
+
+    # Check if p is on or inside the bounding sphere. If not, it must be
+    # part of the new boundary.
+    R, C, P_new = B_min_sphere(P_new, B)
+    if np.isnan(R) or np.isinf(R) or R < eps:
+        chk = True
+    else:
+        chk = np.linalg.norm(p - C) > (R + eps)
+
+    if chk:
+        if len(B) == 0:
+            B = np.array([p])
+        else:
+            B = np.array(np.insert(B, 0, p, axis=0))
+        R, C, _ = B_min_sphere(P_new, B)
+        P = np.insert(P_new.copy(), 0, p, axis=0)
+    return R, C, P
+
+
+def bounding_sphere(hull, points):
+    vertices = [points[i] for i in hull.vertices]
+    vertices = np.unique(vertices, axis=0)
+
+    vertices = np.random.permutation(vertices)
+
+    num_vertices = len(vertices)
+
+    if num_vertices <= 4:
+        R, C = fit_2_points(vertices)
+        return R, C, vertices
+
+    try:
+        R, C, _ = B_min_sphere(vertices, [])
+
+        # Coordiantes of the points used to compute parameters of the
+        # minimum bounding sphere
+        D = np.sum(np.square(vertices - C), axis=1)
+        idx = np.argsort(D - R ** 2)
+        D = D[idx]
+        Xb = vertices[idx[:5]]
+        D = D[:5]
+        Xb = Xb[D < 1E-6]
+        idx = np.argsort(Xb[:, 0])
+        Xb = Xb[idx]
+        return R, C, Xb
+    except:
+        raise Exception
+
+
+def find_min_bounding_sphere(path, L):
+    v = read_mrc(path)
+
+    points = []
+    density_max = v.max()
+    contour_level = L * density_max
+    for ijk in np.ndindex(v.shape):
+        if v[ijk] >= contour_level:
+            points.append([float(ijk[0]), float(ijk[1]), float(ijk[2])])
+
+    R, C, Xb = exact_min_bound_sphere_3D(points)
+    #print(R)
+    #print(C)
+    #print(Xb)
+    #print(R, C, Xb)
+    return R, C
+
+
+
+#R, C = find_min_bounding_sphere('./1W6T.mrc', 0.2)
+#print(R, C)
+
+
+
---
+++ aitom/simulation/tomogram/single_bounding_sphere/simulate.py
@@ -0,0 +1,216 @@
+import pack
+import boundingSphere
+import numpy as np
+from Bio.PDB import *
+#import math
+import mrcfile
+#from mpl_toolkits.mplot3d import Axes3D
+#import matplotlib.pyplot as plt
+#from itertools import product, combinations
+from mayavi.mlab import *
+from random import shuffle
+import copy
+import miniball
+
+
+
+def read_mrc(mrc_path):
+    with mrcfile.open(mrc_path) as mrc:
+        v = mrc.data
+    return v
+
+def get_mass(path):
+    atom_count = 0
+    parser = PDBParser(PERMISSIVE=1)
+    structure = parser.get_structure("X", path)
+    for atom in structure.get_atoms():
+        atom_count += 1
+
+    return atom_count
+    """visited = {}
+    for line in open(path):
+        l = line.split()
+        id = l[0]
+        if id == 'ATOM':
+            type = l[2]
+            if type == 'CA':
+                residue = l[3]
+                type_of_chain = l[4]
+                atom_count = int(l[5])
+                position = l[6:8]
+                if atom_count >= 0:
+                    if type_of_chain not in visited:
+                        visited[type_of_chain] = 1
+                        return atom_count
+                        #print (residue,type_of_chain,atom_count,' '.join(position))
+    #return atom_count"""
+
+
+def find_bounding_sphere(mrc, L):
+    v = read_mrc(mrc)
+
+    points = []
+    density_max = v.max()
+    contour_level = L * density_max
+    for ijk in np.ndindex(v.shape):
+        if v[ijk] >= contour_level:
+            points.append([float(ijk[0]), float(ijk[1]), float(ijk[2])])
+
+    points = np.asarray(points)
+
+    #return boundingSphere.find_min_bounding_sphere(points)
+    return miniball.Miniball(points)
+
+
+def get_distribution(proteins, Ntot):
+    num = len(proteins)
+    f = np.random.random(num)
+    f /= f.sum()
+    dist = np.random.multinomial(Ntot, f)
+    conf = []
+
+    for i in range(len(dist)):
+        protein = proteins[i]
+        mrc = 'mrc1/' + protein + '.mrc'
+        pdb = 'pdb/' + protein + '.pdb'
+        R, C = find_bounding_sphere(mrc, 0.2)
+        #mass = np.random.randint(0,100)
+        mass = get_mass(pdb)
+
+        num_occurrence = dist[i]
+
+        for j in range(num_occurrence):
+            conf.append({'r': R, 'mass': mass, 'id': protein})
+
+    return conf
+
+
+
+def dist(proteins, data):
+    conf = []
+    for protein in proteins:
+        for j in range(20):
+            pro = copy.deepcopy(data[protein])
+            pro['mass'] += j
+            conf.append(pro)
+    
+    return conf
+
+
+leng = 1200
+
+p1a1s = {'r':  3.9597, 'mass': 35098.5, 'id': '1A1S', 'c': 'b'}
+p1eqr = {'r': 8.2916, 'mass': 198063.6, 'id': '1EQR', 'c': 'g'}
+p1gyt = {'r': 12.2575, 'mass': 661785.41, 'id': '1GYT', 'c': 'r'}
+p1kyi = {'r': 10.2705, 'mass': 834906.19, 'id': '1KYI', 'c': 'c'}
+p1vpx = {'r': 9.0134, 'mass': 516090.03, 'id': '1VPX', 'c': 'm'}
+p1w6t = {'r': 4.767, 'mass': 97702.8, 'id': '1W6T', 'c': 'y'}
+#p2awb = {'r': 11.4888, 'mass': 4293422.15, 'id': '2AWB'}
+p2byu = {'r': 6.6332, 'mass': 148515.59, 'id': '2BYU', 'c': 'k'}
+p2gls = {'r': 8.3555, 'mass': 623832.89, 'id': '2GLS', 'c': 'w'}
+p2ldb = {'r': 6.6077, 'mass': 172942.04, 'id': '2LDB', 'c': 'b'}
+p3dy4 = {'r': 9.7921, 'mass': 705072, 'id': '3DY4', 'c': 'g'}
+p1bxr = {'r': 12.0576, 'mass': 644905.85, 'id': '1BXR', 'c': 'r'}
+p1f1b = {'r': 6.7334, 'mass': 103551.4, 'id': '1F1B', 'c': 'c'}
+p1kp8 = {'r': 9.8714, 'mass': 810237.09, 'id': '1KP8', 'c': 'm'}
+p1qo1 = {'r': 10.6771, 'mass': 448855.91, 'id': '1QO1', 'c': 'y'}
+p1vrg = {'r': 6.8439, 'mass': 352304.08, 'id': '1VRG', 'c': 'k'}
+p1yg6 = {'r': 6.8267, 'mass': 303872.45, 'id': '1YG6', 'c': 'w'}
+p2bo9 = {'r': 6.6997, 'mass': 123008.66, 'id': '2BO9', 'c': 'b'}
+p2gho = {'r': 7.6688, 'mass': 333287.4, 'id': '2GHO', 'c': 'g'}
+p2h12 = {'r': 7.0397, 'mass': 298564.65, 'id': '2H12', 'c': 'r'}
+p2rec = {'r': 7.0887, 'mass': 228099.61, 'id': '2REC', 'c': 'c'}
+
+proteins = ['1a1s', '1bxr', '1eqr', '1f1b', '1gyt', '1kp8', '1kyi', '1qo1', '1vpx', '1vrg', '1w6t', '1yg6', '2bo9', '2byu', '2gho', '2gls', '2h12', '2ldb', '2rec', '3dy4']
+data = {'1a1s': p1a1s, '1eqr': p1eqr, '1gyt': p1gyt, '1kyi': p1kyi, '1vpx': p1vpx, '1w6t': p1w6t, '2byu': p2byu,
+'2gls': p2gls, '2ldb': p2ldb, '3dy4': p3dy4, '1bxr': p1bxr, '1f1b': p1f1b, '1kp8': p1kp8, '1qo1': p1qo1,
+'1vrg': p1vrg, '1yg6': p1yg6, '2bo9': p2bo9, '2gho': p2gho, '2h12': p2h12, '2rec': p2rec}
+#proteins = ['1A1S', '1BXR', '1EQR', '1FNT', '1KP8', '1LB3', '1VPX', '1W6T', '2AWB', '2BYU', '2H12', '2REC', '3GPT', '4V4A', '5L5A', '1AON', '1DPB', '1F1B', '1GYT', '1KYI', '1QO1', '1VRG', '1YG6', '2BO9', '2GHO', '2IDB', '3DY4', '3K7A', '4V4Q']
+#conf = get_distribution(proteins, 400)
+
+conf = dist(proteins, data)
+
+#shuffle(conf)
+print(conf)
+
+cx = pack.do_packing(conf, op={'box':{'x':leng, 'y':leng, 'z':leng}, 'temprature':3000, 'temprature decrease':100, 'step':5, 'recent_scores number':50, 'recent_scores slope min':0.001, 'min score':0.1}, pymol_file_name='./t.pym')
+
+l = cx['conf']
+print(l)
+print(cx['inside_box_num'])
+print(cx['score'])
+print(cx['temprature'])
+
+arr = np.asarray(l)
+np.save("particles.npy", arr)
+
+xs = []
+ys = []
+zs = []
+rs = []
+
+for protein in l:
+    xs.append(protein['x'][0])
+    ys.append(protein['x'][1])
+    zs.append(protein['x'][2])
+    rs.append(protein['r'])
+
+points3d(xs, ys, zs, rs)
+show()
+
+
+'''def WireframeSphere(centre, radius,
+                    n_meridians=20, n_circles_latitude=None):
+    """
+    Create the arrays of values to plot the wireframe of a sphere.
+
+    Parameters
+    ----------
+    centre: array like
+        A point, defined as an iterable of three numerical values.
+    radius: number
+        The radius of the sphere.
+    n_meridians: int
+        The number of meridians to display (circles that pass on both poles).
+    n_circles_latitude: int
+        The number of horizontal circles (akin to the Equator) to display.
+        Notice this includes one for each pole, and defaults to 4 or half
+        of the *n_meridians* if the latter is larger.
+
+    Returns
+    -------
+    sphere_x, sphere_y, sphere_z: arrays
+        The arrays with the coordinates of the points to make the wireframe.
+        Their shape is (n_meridians, n_circles_latitude).
+
+    Examples
+    --------
+    >>> fig = plt.figure()
+    >>> ax = fig.gca(projection='3d')
+    >>> ax.set_aspect("equal")
+    >>> sphere = ax.plot_wireframe(*WireframeSphere(), color="r", alpha=0.5)
+    >>> fig.show()
+
+    >>> fig = plt.figure()
+    >>> ax = fig.gca(projection='3d')
+    >>> ax.set_aspect("equal")
+    >>> frame_xs, frame_ys, frame_zs = WireframeSphere()
+    >>> sphere = ax.plot_wireframe(frame_xs, frame_ys, frame_zs, color="r", alpha=0.5)
+    >>> fig.show()
+    """
+    if n_circles_latitude is None:
+        n_circles_latitude = max(n_meridians/2, 4)
+    u, v = np.mgrid[0:2*np.pi:n_meridians*1j, 0:np.pi:n_circles_latitude*1j]
+    sphere_x = centre[0] + radius * np.cos(u) * np.sin(v)
+    sphere_y = centre[1] + radius * np.sin(u) * np.sin(v)
+    sphere_z = centre[2] + radius * np.cos(v)
+    return sphere_x, sphere_y, sphere_z
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+#ax.set_aspect('equal')
+for sphere in l:
+    ax.plot_wireframe(*WireframeSphere(sphere['x'], sphere['r']), color=sphere['c'], alpha=0.5)
+fig.show()
+plt.show()'''
