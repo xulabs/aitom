6ea2e1223c8f720a16b5fecc50b79b921cf0b015
Cerry <lihuiyu20@163.com>
2020-07-12 18:53:28 +0800

    first commit

---
+++ aitom/classify/deep/supervised/cnn/mult_task/model.py
@@ -0,0 +1,238 @@
+import keras
+from keras.layers import Input, Dense, Conv3D, MaxPooling3D, UpSampling3D, Cropping3D, Dropout, Add, Reshape, \
+    concatenate
+from keras.models import Sequential, Model, load_model
+from keras.layers.core import Activation
+import numpy as np
+
+# Model  2AWB
+def FCN8(img_shape, class_n=None):
+    input_shape = (None, img_shape[0], img_shape[1], img_shape[2], 1)
+
+    # use relu activation for hidden layer to guarantee non-negative outputs are passed to the max pooling layer. In such case, as long as the output layer is linear activation, the network can still accomodate negative image intendities, just matter of shift back using the bias term
+    input_img = Input(shape=input_shape[1:])
+
+    conv1_1 = Conv3D(32, 3, padding='same', activation='relu')(input_img)
+    conv1_2 = Conv3D(32, 3, padding='same', activation='relu')(conv1_1)
+    pool1 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv1_2)
+
+    conv2_1 = Conv3D(64, 3, padding='same', activation='relu')(pool1)
+    conv2_2 = Conv3D(64, 3, padding='same', activation='relu')(conv2_1)
+    pool2 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv2_2)
+
+    conv3_1 = Conv3D(128, 3, padding='same', activation='relu')(pool2)
+    conv3_2 = Conv3D(128, 3, padding='same', activation='relu')(conv3_1)
+    # if use_dilated:
+    pool3 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv3_2)
+
+    # fully conv
+    fc4 = Conv3D(1024, 1, padding='same', activation='relu')(pool3)
+    drop5 = Dropout(0.7)(fc4)
+
+    fc6 = Conv3D(1024, 1, padding='same', activation='relu')(drop5)
+    drop7 = Dropout(0.7)(fc6)
+
+    # upsampling
+    score1 = Conv3D(2, 1, padding='same')(drop7)
+    upscore1 = UpSampling3D((2, 2, 2))(score1)
+    score_pool1 = Conv3D(2, 1, padding='same')(pool2)
+    # upscore1,score_pool1 =crop( upscore1 , score_pool1 , input_img)
+
+    score2 = Add()([upscore1, score_pool1])
+    upscore2 = UpSampling3D((2, 2, 2))(score2)
+    score_pool2 = Conv3D(2, 1, padding='same')(pool1)
+    # upscore2,score_pool2 =crop( upscore2 , score_pool2 , input_img)
+
+    score3 = Add()([upscore2, score_pool2])
+    upscore3 = UpSampling3D((2, 2, 2))(score3)
+    # upscore4=checker(upscore3,input_img)
+
+    output1 = Reshape((np.prod(img_shape), 2))(upscore3)
+    output = Activation('softmax')(output1)
+
+    model = Model(inputs=input_img, outputs=output)
+
+    return model
+
+# Model  2AWB
+def FCN1(img_shape, class_n=None):
+    input_shape = (None, img_shape[0], img_shape[1], img_shape[2], 1)
+
+    # use relu activation for hidden layer to guarantee non-negative outputs are passed to the max pooling layer. In such case, as long as the output layer is linear activation, the network can still accomodate negative image intendities, just matter of shift back using the bias term
+    input_img = Input(shape=input_shape[1:])
+
+    conv1_1 = Conv3D(32, 3, padding='same', activation='relu')(input_img)
+    conv1_2 = Conv3D(32, 3, padding='same', activation='relu')(conv1_1)
+    pool1 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv1_2)
+
+    conv2_1 = Conv3D(64, 3, padding='same', activation='relu')(pool1)
+    conv2_2 = Conv3D(64, 3, padding='same', activation='relu')(conv2_1)
+    pool2 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv2_2)
+
+    conv3_1 = Conv3D(128, 3, padding='same', activation='relu')(pool2)
+    conv3_2 = Conv3D(128, 3, padding='same', activation='relu')(conv3_1)
+    # if use_dilated:
+    pool3 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv3_2)
+
+    # fully conv
+    fc4 = Conv3D(1024, 1, padding='same', activation='relu')(pool3)
+    drop5 = Dropout(0.7)(fc4)
+
+    fc6 = Conv3D(1024, 1, padding='same', activation='relu')(drop5)
+    drop7 = Dropout(0.7)(fc6)
+
+    # upsampling
+    score1 = Conv3D(2, 1, padding='same')(drop7)
+    upscore1 = UpSampling3D((2, 2, 2))(score1)
+
+    # upscore1,score_pool1 =crop( upscore1 , score_pool1 , input_img)
+
+    score2 = Conv3D(2, 1, padding='same')(upscore1)
+    score3 = Conv3D(2, 1, padding='same')(score2)
+    upscore2 = UpSampling3D((2, 2, 2))(score3)
+
+    # upscore2,score_pool2 =crop( upscore2 , score_pool2 , input_img)
+
+    score4 = Conv3D(2, 1, padding='same')(upscore2)
+    score5 = Conv3D(2, 1, padding='same')(score4)
+    upscore3 = UpSampling3D((2, 2, 2))(score4)
+    # upscore4=checker(upscore3,input_img)
+
+    output1 = Reshape((np.prod(img_shape), 2))(upscore3)
+    output = Activation('softmax')(output1)
+
+    model = Model(inputs=input_img, outputs=output)
+
+    return model
+
+def FCN_ed(img_shape, class_n=None):
+    input_shape = (None, img_shape[0], img_shape[1], img_shape[2], 1)
+    # use relu activation for hidden layer to guarantee non-negative outputs are passed to the max pooling layer. In such case, as long as the output layer is linear activation, the network can still accomodate negative image intendities, just matter of shift back using the bias term
+    input_img = Input(shape=input_shape[1:])
+
+    conv1_1 = Conv3D(32, 3, padding='same', activation='relu')(input_img)
+    conv1_2 = Conv3D(32, 3, padding='same', activation='relu')(conv1_1)
+    pool1 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv1_2)
+
+    conv2_1 = Conv3D(64, 3, padding='same', activation='relu')(pool1)
+    conv2_2 = Conv3D(64, 3, padding='same', activation='relu')(conv2_1)
+    pool2 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv2_2)
+
+    conv3_1 = Conv3D(128, 3, padding='same', activation='relu')(pool2)
+    conv3_2 = Conv3D(128, 3, padding='same', activation='relu')(conv3_1)
+    # if use_dilated:
+    pool3 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv3_2)
+
+    # # fully conv
+    # fc4 = Conv3D(1024, 1, padding='same', activation='relu')(pool3)
+    # drop5 = Dropout(0.7)(fc4)
+
+    # fc6 = Conv3D(1024, 1, padding='same', activation='relu')(drop5)
+    # drop7 = Dropout(0.7)(fc6)
+
+    # upsampling
+    score1_1 = Conv3D(128, 1, padding='same', activation='relu')(pool3)
+    upscore1 = UpSampling3D((2, 2, 2))(score1_1)
+
+    score_pool1 = Conv3D(128, 1, padding='same')(pool2)
+    score1 = Add()([upscore1, score_pool1])
+
+    score2_1 = Conv3D(64, 1, padding='same', activation='relu')(score1)
+    upscore2 = UpSampling3D((2, 2, 2))(score2_1)
+
+    score_pool2 = Conv3D(64, 1, padding='same')(pool1)
+    score2 = Add()([upscore2, score_pool2])
+
+    score3_1 = Conv3D(32, 1, padding='same', activation='relu')(score2)
+    upscore3 = UpSampling3D((2, 2, 2))(score3_1)
+    # upscore4=checker(upscore3,input_img)
+    upscore4 = Conv3D(2, 1, padding='same')(upscore3)
+    output1 = Reshape((np.prod(img_shape), 2))(upscore4)
+    output = Activation('softmax')(output1)
+
+    model = Model(inputs=input_img, outputs=output)
+
+    return model
+
+# Model  2AWB
+def FCN_aspp(img_shape, class_n=None):
+    input_shape = (None, img_shape[0], img_shape[1], img_shape[2], 1)
+
+    # use relu activation for hidden layer to guarantee non-negative outputs are passed to the max pooling layer. In such case, as long as the output layer is linear activation, the network can still accomodate negative image intendities, just matter of shift back using the bias term
+    input_img = Input(shape=input_shape[1:])
+
+    conv1_1 = Conv3D(32, 3, padding='same', activation='relu')(input_img)
+    conv1_2 = Conv3D(32, 3, padding='same', activation='relu')(conv1_1)
+    pool1 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv1_2)
+
+    conv2_1 = Conv3D(64, 3, padding='same', activation='relu')(pool1)
+    conv2_2 = Conv3D(64, 3, padding='same', activation='relu')(conv2_1)
+    pool2 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv2_2)
+
+    conv3_1 = Conv3D(128, 3, padding='same', activation='relu', dilation_rate=2)(pool2)
+
+    fcp1 = Conv3D(128, 1, padding='same', activation='relu')(conv3_1)
+    fcp2 = Conv3D(128, 3, padding='same', activation='relu', dilation_rate=2)(conv3_1)
+    fcp3 = Conv3D(128, 3, padding='same', activation='relu', dilation_rate=4)(conv3_1)
+    cancate = concatenate([fcp1, fcp2, fcp3, conv3_1])
+
+    # upsampling
+    upscore1 = UpSampling3D((4, 4, 4))(cancate)
+
+    # score_pool1=Conv3D(512, 1, padding='same')(pool1)
+    # score1=Add()([upscore1,score_pool1])
+
+    # upscore2=UpSampling3D((2,2,2))(score1)
+
+    # upscore4=checker(upscore3,input_img)
+    upscore4 = Conv3D(2, 1, padding='same')(upscore1)
+    output1 = Reshape((np.prod(img_shape), 2))(upscore4)
+    output = Activation('softmax')(output1)
+
+    model = Model(inputs=input_img, outputs=output)
+
+    return model
+
+# test the dilated conv
+def FCN_ed2(img_shape, class_n=None):
+    input_shape = (None, img_shape[0], img_shape[1], img_shape[2], 1)
+
+    # use relu activation for hidden layer to guarantee non-negative outputs are passed to the max pooling layer. In such case, as long as the output layer is linear activation, the network can still accomodate negative image intendities, just matter of shift back using the bias term
+    input_img = Input(shape=input_shape[1:])
+
+    conv1_1 = Conv3D(32, 3, padding='same', activation='relu')(input_img)
+    conv1_2 = Conv3D(32, 3, padding='same', activation='relu')(conv1_1)
+    pool1 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv1_2)
+
+    conv2_1 = Conv3D(64, 3, padding='same', activation='relu')(pool1)
+    conv2_2 = Conv3D(64, 3, padding='same', activation='relu')(conv2_1)
+    pool2 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv2_2)
+
+    conv3_1 = Conv3D(128, 3, padding='same', activation='relu')(pool2)
+    conv3_2 = Conv3D(128, 3, padding='same', activation='relu')(conv3_1)
+    # if use_dilated:
+    pool3 = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), padding='same')(conv3_2)
+
+    # upsampling
+    score1_1 = Conv3D(128, 1, padding='same', activation='relu', dilation_rate=2)(pool3)
+    upscore1 = UpSampling3D((2, 2, 2))(score1_1)
+
+    score_pool1 = Conv3D(128, 1, padding='same')(pool2)
+    score1 = Add()([upscore1, score_pool1])
+
+    score2_1 = Conv3D(64, 1, padding='same', activation='relu')(score1)
+    upscore2 = UpSampling3D((2, 2, 2))(score2_1)
+
+    score_pool2 = Conv3D(64, 1, padding='same')(pool1)
+    score2 = Add()([upscore2, score_pool2])
+
+    score3_1 = Conv3D(32, 1, padding='same', activation='relu')(score2)
+    upscore3 = UpSampling3D((2, 2, 2))(score3_1)
+    # upscore4=checker(upscore3,input_img)
+    upscore4 = Conv3D(2, 1, padding='same')(upscore3)
+    output1 = Reshape((np.prod(img_shape), 2))(upscore4)
+    output = Activation('softmax')(output1)
+
+    model = Model(inputs=input_img, outputs=output)
+
+    return model
---
+++ aitom/segmentation/saliency/deep/unsupervised/pub_salnet/train.py
@@ -0,0 +1,105 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+import datetime
+import os
+import numpy as np
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+
+from network import Unet
+from dataset import CustomDataset
+
+cfg = {'PicaNet': "GGLLL",
+       'Size': [4, 4, 4, 4, 8, 6],
+       'Channel': [1024, 512, 512, 256, 128, 64],
+       'loss_ratio': [0.5, 0.5, 0.5, 0.8, 0.8, 1]}
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    config = {
+        'mode': 'self_train',  # ['pre_train','self_train','test']
+        'lr': 0.001,  # learning_rate, default = 0.001
+        'lr_decay': 0.1,  # Learning rate decrease by lr_decay time per decay_step, default = 0.1
+        'decay_step': 7000,  # Learning rate decrease by lr_decay time per decay_step,  default = 7000
+        'batch_size': 20,  # batchsize, default = 1
+        'epoch': 20,  # epochs, default = 20
+        'dataset': '',  # Directory of your Dataset
+        'load': None,  # Directory of pre-trained model
+        'display_freq': 1000,  # display_freq to display result image on Tensorboard
+    }
+    torch.cuda.manual_seed_all(1234)
+    torch.manual_seed(1234)
+    start_iter = 0
+    now = datetime.datetime.now()
+    start_epo = 0
+
+    duts_dataset = CustomDataset(config['dataset'])
+    model = Unet(cfg,config['mode']).cuda()
+    # vgg = torchvision.models.vgg16(pretrained=True)
+    # model.encoder.seq.load_state_dict(vgg.features.state_dict())
+    # del vgg
+
+    if config['load'] is not None:
+        state_dict = torch.load(config['load'], map_location='cuda')
+
+        start_iter = 1#int(config['load'].split('epo_')[1].strip('step.ckpt')) + 1
+        start_epo = 0#int(config['load'].split('/')[3].split('epo')[0])
+        now = time.strftime('%Y%m%d-%H%M%S', time.localtime())
+
+        print("Loading Model from {}".format(config['load']))
+        print("Start_iter : {}".format(start_iter))
+        model.load_state_dict(state_dict)
+        for cell in model.decoder:
+            if cell.mode == 'G':
+                cell.picanet.renet.vertical.flatten_parameters()
+                cell.picanet.renet.horizontal.flatten_parameters()
+        print('Loading_Complete')
+
+    # Optimizer Setup
+    learning_rate = config['lr'] * (config['lr_decay'] ** (start_iter // config['decay_step']))
+    opt_en = torch.optim.SGD(model.encoder.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0005)
+    opt_dec = torch.optim.SGD(model.decoder.parameters(), lr=learning_rate * 10, momentum=0.9, weight_decay=0.0005)
+    # Dataloader Setup
+    dataloader = DataLoader(duts_dataset, config['batch_size'], shuffle=True, num_workers=0)
+    # Logger Setup
+    os.makedirs(os.path.join('log', now.strftime('%m%d%H%M')), exist_ok=True)
+    weight_save_dir = os.path.join('models', 'state_dict', now.strftime('%m%d%H%M'))
+    os.makedirs(os.path.join(weight_save_dir), exist_ok=True)
+    writer = SummaryWriter(os.path.join('log', now.strftime('%m%d%H%M')))
+    iterate = start_iter
+    for epo in range(start_epo, config['epoch']):
+        print("\nEpoch : {}".format(epo))
+        for i, batch in enumerate(tqdm(dataloader)):
+            if i > 10:
+                break
+            opt_dec.zero_grad()
+            opt_en.zero_grad()
+            img = batch['image'].cuda()
+            mask = batch['mask'].cuda()
+            pred, loss = model(img, mask)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 100)
+            opt_dec.step()
+            opt_en.step()
+            writer.add_scalar('loss', float(loss), global_step=iterate)
+            if iterate % config['display_freq'] == 0:
+                for masked in pred:
+                    writer.add_image('{}'.format(masked.size()[2]), masked[0], global_step=iterate)
+                writer.add_image('GT', mask[0], iterate)
+                writer.add_image('Image', img[0], iterate)
+
+            if iterate % 200 == 0:
+                if i != 0:
+                    torch.save(model.state_dict(), os.path.join(weight_save_dir, '{}epo_{}step.ckpt'.format(epo, iterate)))
+            if iterate % 1000 == 0 and i != 0:
+                for file in weight_save_dir:
+                    if '00' in file and '000' not in file:
+                        os.remove(os.path.join(weight_save_dir, file))
+            if i + epo * len(dataloader) % config['decay_step'] == 0 and i != 0:
+                learning_rate *= config[lr_decay]
+                opt_en = torch.optim.SGD(model.encoder.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0005)
+                opt_dec = torch.optim.SGD(model.decoder.parameters(), lr=learning_rate * 10, momentum=0.9, weight_decay=0.0005)
+            iterate += config['batch_size']
+            del loss
+        start_iter = 0
