b29b44d741a3a73f310e7b60a3005151c6a9061b
李冉 <liran@lirandeMacBook-Pro.local>
2020-08-18 10:14:24 +0800

    add files to few_shot/protonet

---
+++ aitom/classify/deep/supervised/cnn/few_shot/protonet/protonets/utils/data.py
@@ -0,0 +1,9 @@
+import protonets.data
+
+def load(opt, splits):
+    if opt['data.dataset'] == 'subtomo':
+        ds = protonets.data.subtomo.load(opt, splits)
+    else:
+        raise ValueError("Unknown dataset: {:s}".format(opt['data.dataset']))
+
+    return ds
