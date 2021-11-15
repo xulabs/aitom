ddd333475f7b2767c815673cce46dca645f33513
Haoyi Zhu <63538191+HaoyiZhu@users.noreply.github.com>
2021-11-16 01:52:12 +0800

    fix a bug in featureL2norm layer (#205)
    * fix a bug
    
    * avoid occupying all memory
    
    * Optimize code hierarchy
    
    * Update train_demo.py
    
    * Update opt.py
    
    * Update FeatureL2Norm.py

--- aitom/align/deep/gum/opt.py
+++ aitom/align/deep/gum/opt.py
@@ -1,8 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Gum-Net Demo Options')
-parser.add_argument('--initial_model', default=True, 
-                    help='whether to use pretrained model', action='store_true')
+parser.add_argument('--build_model', default=False, 
+                    help='whether to use pretrained model or build a new model', action='store_true')
 parser.add_argument('--initial_lr', default=1e-7, type=float,
                     help='initial learning rate')
 parser.add_argument('--data_path', default='gum_demo_data.pickle', type=str,
