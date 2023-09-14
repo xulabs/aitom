efa87333f971e9eb0dd4b53f31140a30011078e7
SineZHAN <46485112+SineZHAN@users.noreply.github.com>
2023-09-13 23:17:13 -0400

    fixed mrc header bug

--- doc/tutorials/023_Denoising_tutorial.py
+++ doc/tutorials/023_Denoising_tutorial.py
@@ -1,3 +1,8 @@
+import os, sys
+print(os.getcwd())
+os.chdir("../..") # Depends on your current dir
+print(os.getcwd())
+sys.path.append(".")
 import aitom.filter.gaussian as G
 import numpy as np
 import aitom.io.mrcfile_proxy as mrcfile_proxy
@@ -7,9 +12,21 @@ import matplotlib.pyplot as plt
 import aitom.filter.anistropic_diffusion.fastaniso as ft
 from numpy.fft import fftn, ifftn, fftshift, ifftshift
 import aitom.image.vol.util as GV
+import mrcfile
 
+save_dir = './Denoising_Result'
 
-def g_denoising(G_type, a, name, gaussian_sigma, save_flag=False):
+def write_with_spacing(data, voxel_spacing, path, overwrite=False):
+    # only for 3D array
+    assert data.ndim == 3
+    data = data.astype(np.float32)
+    # transpose data according to tomominer.image.vol.eman2_util.em2numpy
+    data = data.transpose([2, 1, 0])
+    with mrcfile.new(path, overwrite=overwrite) as m:
+        m.set_data(data)
+        m.voxel_size = voxel_spacing
+
+def g_denoising(G_type, a, voxel_spacing, name, gaussian_sigma, save_flag=False):
     b_time = time.time()
 
     if G_type == 1:
@@ -25,18 +42,19 @@ def g_denoising(G_type, a, name, gaussian_sigma, save_flag=False):
     if save_flag:
         img = (a[:, :, int(a.shape[2] / 2)]).copy()
         # TODO: Change the image and tomogram saving path
-        img_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Gaussian/' + str(name) + '_G=' + \
+        img_path = save_dir + '/Gaussian/' + str(name) + '_G=' + \
                    str(gaussian_sigma) + '_type=' + str(G_type) + '.png'
         plt.imsave(img_path, img, cmap='gray')
 
-        mrc_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Gaussian/' + str(name) + '_G=' + \
+        mrc_path = save_dir + '/Gaussian/' + str(name) + '_G=' + \
                    str(gaussian_sigma) + '_type=' + str(G_type) + '.mrc'
-        io_file.put_mrc_data(a, mrc_path)
+        #io_file.put_mrc_data(a, mrc_path)
+        write_with_spacing(a, voxel_spacing, mrc_path)
 
         return img
 
 
-def bandpass_denoising(a, name, save_flag=False):
+def bandpass_denoising(a, voxel_spacing, name, save_flag=False):
     b_time = time.time()
     grid = GV.grid_displacement_to_center(a.shape, GV.fft_mid_co(a.shape))
     rad = GV.grid_distance_to_center(grid)
@@ -51,23 +69,24 @@ def bandpass_denoising(a, name, save_flag=False):
     #perform FFT and filter the data with the mask and then transform the filtered data back
     vf = ifftn(ifftshift((fftshift(fftn(a)) * curve)))
     vf = np.real(vf)
-
+    
     end_time = time.time()
     print('Bandpass de-noise takes', end_time - b_time, 's')
 
     if save_flag:
         img = (vf[:, :, int(vf.shape[2] / 2)]).copy()
         # TODO: Change the image and tomogram saving path
-        img_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Bandpass/' + str(name) + '_BP.png'
+        img_path = save_dir + '/Bandpass/' + str(name) + '_BP.png'
         plt.imsave(img_path, img, cmap='gray')
 
-        mrc_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Bandpass/' + str(name) + '_BP.mrc'
-        io_file.put_mrc_data(vf, mrc_path)
+        mrc_path = save_dir + '/Bandpass/' + str(name) + '_BP.mrc'
+        #io_file.put_mrc_data(vf, mrc_path)
+        write_with_spacing(vf, voxel_spacing, mrc_path)
 
         return img
 
 
-def anistropic_diffusion(a, niter, kappa, gamma, step, option, ploton, name, save_flag=False):
+def anistropic_diffusion(a, voxel_spacing, niter, kappa, gamma, step, option, ploton, name, save_flag=False):
     b_time = time.time()
 
     #see http://pastebin.com/sBsPX4Y7 for details
@@ -79,13 +98,14 @@ def anistropic_diffusion(a, niter, kappa, gamma, step, option, ploton, name, sav
     if save_flag:
         af_fimg = (af[:, :, int(af.shape[2] / 2)]).copy()
         # TODO: Change the image and tomogram saving path
-        af_fimg_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Anistropic_Diffusion/' + str(name) + \
+        af_fimg_path = save_dir + '/Anistropic_Diffusion/' + str(name) + \
                        '_AD_i=' + str(niter) + '_k=' + str(kappa) + '_g=' + str(gamma) + '.png'
         plt.imsave(af_fimg_path, af_fimg, cmap='gray')
 
-        mrc_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Anistropic_Diffusion/' + str(name) + \
+        mrc_path = save_dir + '/Anistropic_Diffusion/' + str(name) + \
                        '_AD_i=' + str(niter) + '_k=' + str(kappa) + '_g=' + str(gamma) + '.mrc'
-        io_file.put_mrc_data(af, mrc_path)
+        #io_file.put_mrc_data(af, mrc_path)
+        write_with_spacing(af, voxel_spacing, mrc_path)
 
         return af_fimg
 
@@ -95,32 +115,37 @@ def diff_compare(oimg, fimg, type):
     dmax = np.amax(dimg)
     dimg = (dimg / dmax) * 255
     # TODO: Change the image saving path
-    img_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Difference_' + str(type) + '.png'
+    img_path = save_dir + '/Difference_' + str(type) + '.png'
     plt.imsave(img_path, dimg, cmap='gray')
 
 
 if __name__ == "__main__":
     # TODO: Change the data path, name and Gussian denoise type
-    path = '/Users/apple/Desktop/Lab/Zach_Project/Sample_Data/aitom_demo_single_particle_tomogram.mrc'
+    path = '../local_scratch/aitom_demo_single_particle_tomogram.mrc'
     name = 'aitom_demo_single_particle_tomogram'
     G_type = 1
 
     # read the volume data as numpy array
     original = mrcfile_proxy.read_data(path)
+
+    mrc_header = io_file.read_mrc_header(path)
+    voxel_spacing = mrc_header['MRC']['xlen'] / mrc_header['MRC']['nx']
+    print(voxel_spacing)
+
     # save a slice of original tomogram
     oimg = (original[:, :, int(original.shape[2] / 2)]).copy()
     # TODO: Change the orginal image saving directory
-    img_path = '/Users/apple/Desktop/Lab/Zach_Project/Denoising_Result/Original.png'
+    img_path = save_dir + '/Original.png'
     plt.imsave(img_path, oimg, cmap='gray')
 
     # perform three diffrent kind of denoising
     # The difference comparison between original tomogram and filtered tomogram is commented out
-    g_fimg = g_denoising(G_type, a=original, name=name, gaussian_sigma=2.5, save_flag=True)
+    g_fimg = g_denoising(G_type, a=original, voxel_spacing=voxel_spacing, name=name, gaussian_sigma=2.5, save_flag=True)
     # diff_compare(oimg, g_fimg, "Gaussian")
 
-    bp_fimg = bandpass_denoising(a=original, name=name, save_flag=True)
+    bp_fimg = bandpass_denoising(a=original, voxel_spacing=voxel_spacing, name=name, save_flag=True)
     # diff_compare(oimg, bp_fimg, "BP")
 
-    af_fimg = anistropic_diffusion(a=original, niter=70, kappa=100, gamma=0.25, step=(4., 4., 4.), option=1,
-                                   ploton=True, name=name, save_flag=True)
+    af_fimg = anistropic_diffusion(a=original, voxel_spacing=voxel_spacing, niter=70, kappa=100, gamma=0.25, 
+            step=(4., 4., 4.), option=1, ploton=True, name=name, save_flag=True)
     # diff_compare(oimg, af_fimg, "AF")
